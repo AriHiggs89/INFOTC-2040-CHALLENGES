96c4c3f6298e7f38abf77e81b1bc820d5ebe239c
Ariana <arianahiggins89@gmail.com>
2021-03-02 21:06:04 -0600

    Add files via upload

---
+++ Crime Analyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrimeAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Crimes crimes = new Crimes(args);
+        }
+    }
+}
